add36e96cb10da7292aac0d04f7fa23f4fdb338a
mickcaff <mcaffery@outlook.com>
2022-09-21 17:14:52 +1000

    ran npm run build

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <h1>Portfolio</h1><br />
       <h2>Under Construction</h2><br />
-      <p>Regards, Mick</p><br/>
+      <p>Regards, Mick</p><br/><br />
       <p>---</p><br/>
       <a href="https://www.linkedin.com/in/mcaffery/" rel='noreferrer' target="__blank" >LinkedIn</a><br/><br/>
       <a href="https://github.com/mickcaff" rel='noreferrer' target="__blank">GitHub</a><br/><br/>
