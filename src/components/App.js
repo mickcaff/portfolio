62eb6c58536bf22e8a69ea1ba5c2fbb9eba4f4cf
mickcaff <mcaffery@outlook.com>
2022-09-21 14:37:27 +1000

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import '../css/App.css';
+import {link} from "react-router-dom"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Portfolio</h1>
+      <h1>Under Construction</h1>
+      <p>Regards, Mick</p><br/>
+      <p>---</p><br/>
+      <a href="https://www.linkedin.com/in/mcaffery/" rel='noreferrer' target="__blank" >LinkedIn</a><br/><br/>
+      <a href="https://github.com/mickcaff" rel='noreferrer' target="__blank">GitHub</a><br/><br/>
+      {/* <a href="/resume.pdf" target="___blank">Resume</a> */}
+    </div>
+  );
+}
+
+export default App;
